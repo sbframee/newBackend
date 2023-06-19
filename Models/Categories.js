543a5c4322a7a38eb8747bb19e06283f0d0bceaa
sbframee <104187401+sbframee@users.noreply.github.com>
2023-06-19 07:39:51 -0700

    update

--- Models/Categories.js
+++ Models/Categories.js
@@ -4,6 +4,9 @@ const CategoriesSchema = new mongoose.Schema({
   name: {
     type: String,
   },
+  group: {
+    type: String,
+  },
 });
 
 module.exports = mongoose.model("categories", CategoriesSchema);
--- Routers/Categories.js
+++ Routers/Categories.js
@@ -9,7 +9,30 @@ const Category = require("../Models/Categories");
       .then(categories => res.json(categories))
       .catch(error => console.log(error));
   });
+
+  router.get('/getGroup', (req, res) => {
+    Category.find({}, { _id: 0, group: 1 })
+      .then(categories => {
+        const groups = categories.map(category => ({ name: category.group }));
+        res.json(groups);
+      })
+      .catch(error => console.log(error));
+  });
+   
+  router.post('/postGroup', async (req, res) => {
+    try {
+      let value = req.body;
+      if (!value) res.json({ success: false, message: "Invalid Data" });
+      value = { ...value };
   
+      let response = await Category.create(value);
+      if (response) {
+        res.json({ success: true, result: response });
+      } else res.json({ success: false, message: "Group Not created" });
+    } catch (err) {
+      res.status(500).json({ success: false, message: err });
+    }
+  });
   
 
   module.exports = router;
